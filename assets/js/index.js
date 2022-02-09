ef7896b580dc5c54b55e7d132c2cb58ad13d3379
Kristian Philip Colville <kristiancolville1@gmail.com>
2022-02-09 20:31:05 +0000

    Add start/reset game function

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,6 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
     const cubes = document.querySelectorAll('.grid div');
     const score = document.querySelector('span');
+    const isRunning = document.querySelector('.start-game');
     // test that the document is loaded
     // and that snake will move if keys pressed
 
@@ -28,15 +29,34 @@ document.addEventListener('DOMContentLoaded', () => {
     let applePos = 0;
     let snakePos = [2, 1, 0];
     let direction = 1;
-    let score = 0;
+    let scoreDisplay = 0;
     let speed = 0.5;
     let intervalTime = 1000;
     let interval = 0;
 
+    // start game function
+    function startGame(){
+
+        // remove the snake class from the div when game starts..
+        snakePos.forEach(index => cubes[index].classList.remove('snake'));
+
+        // reset these values from above
+        position = 0;
+        applePos = 0;
+        snakePos = [2, 1, 0];
+        direction = 1;
+        scoreDisplay = 0;
+        speed = 0.5;
+        intervalTime = 1000;
+        interval = 0; 
+
+        // add the snake back to the game after values are reset
+        snakePos.forEach(index => cubes[index].classList.add('snake'));
+    }
 
 
     document.addEventListener('keyup', isKeyPressed);
-
+    isRunning.addEventListener('click', startGame);
 
 }
 );
