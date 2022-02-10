e95d69e3cce99cae928d686fa79bb8250847a43b
Kristian Philip Colville <kristiancolville1@gmail.com>
2022-02-10 06:20:31 +0000

    Add snake moves and directions

--- assets/js/index.js
+++ assets/js/index.js
@@ -7,22 +7,6 @@ document.addEventListener('DOMContentLoaded', () => {
 
     const snake = document.querySelectorAll('.snake');
 
-    // we want to test if keys pressed work
-    function isKeyPressed(yes){
-        if(yes.keyCode === 38){
-            alert('up');
-        }
-        else if(yes.keyCode === 37){
-            alert('left');
-        }
-        else if(yes.keyCode === 39){
-            alert('right');
-        }
-        else if(yes.keyCode === 40){
-            alert('down');
-        }
-    }
-
     // Now we want to test that the snake moves if keys pressed
     const width = 10;
     let position = 0;
@@ -52,8 +36,48 @@ document.addEventListener('DOMContentLoaded', () => {
 
         // add the snake back to the game after values are reset
         snakePos.forEach(index => cubes[index].classList.add('snake'));
+        interval = setInterval(snakeMoves, intervalTime);
     }
 
+    // Outcomes depending on the direction
+    function snakeMoves(){
+
+        // adding the snake to front and remove from the end
+        const snakeTail = snakePos.pop();
+        cubes[tail].classList.remove('snake');
+        snakePos.unshift(snakePos[0] + direction);
+
+
+
+        cubes[snakePos[0]].classList.add('snake');
+    }
+
+
+
+
+
+    // we want to test if keys pressed work
+    // then assign direction of navigation to keys
+    function isKeyPressed(yes){
+        cubes[position].classList.remove('snake');
+
+        if(yes.keyCode === 38){
+            // alert('up');
+            direction = -width;
+        }
+        else if(yes.keyCode === 37){
+            // alert('left');
+            direction = -1;
+        }
+        else if(yes.keyCode === 39){
+            // alert('right');
+            direction = +1;
+        }
+        else if(yes.keyCode === 40){
+            // alert('down');
+            direction = +width;
+        }
+    }
 
     document.addEventListener('keyup', isKeyPressed);
     isRunning.addEventListener('click', startGame);
