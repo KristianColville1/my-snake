a45e2bc554a72f5b515ed0ab4a42c4dffd59f96d
Kristian Philip Colville <kristiancolville1@gmail.com>
2022-02-10 06:44:37 +0000

    Fix bug in start game function so snake moves

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,42 +1,41 @@
 document.addEventListener('DOMContentLoaded', () => {
     const cubes = document.querySelectorAll('.grid div');
-    const score = document.querySelector('span');
+    const scoreDisplay = document.querySelector('span');
     const isRunning = document.querySelector('.start-game');
-    // test that the document is loaded
-    // and that snake will move if keys pressed
-
-    const snake = document.querySelectorAll('.snake');
-
-    // Now we want to test that the snake moves if keys pressed
     const width = 10;
-    let position = 0;
+    let lastIndex = 0;
     let applePos = 0;
     let snakePos = [2, 1, 0];
     let direction = 1;
-    let scoreDisplay = 0;
-    let speed = 0.5;
+    let score = 0;
+    let speed = 1;
     let intervalTime = 1000;
     let interval = 0;
 
+    
     // start game function
     function startGame(){
-
+        
         // remove the snake class from the div when game starts..
         snakePos.forEach(index => cubes[index].classList.remove('snake'));
 
+        // remove the apple class from the div when game starts..
+        cubes[applePos].classList.remove('apple');
+        clearInterval(interval);
+
         // reset these values from above
-        position = 0;
-        applePos = 0;
-        snakePos = [2, 1, 0];
+        
+        score = 0;
         direction = 1;
-        scoreDisplay = 0;
-        speed = 0.5;
+        scoreDisplay.innerText = score;
         intervalTime = 1000;
-        interval = 0; 
+        snakePos = [2, 1, 0];
+        lastIndex = 0;
 
         // add the snake back to the game after values are reset
         snakePos.forEach(index => cubes[index].classList.add('snake'));
         interval = setInterval(snakeMoves, intervalTime);
+        
     }
 
     // Outcomes depending on the direction
@@ -44,7 +43,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
         // adding the snake to front and remove from the end
         const snakeTail = snakePos.pop();
-        cubes[tail].classList.remove('snake');
+        cubes[snakeTail].classList.remove('snake');
         snakePos.unshift(snakePos[0] + direction);
 
 
@@ -59,7 +58,7 @@ document.addEventListener('DOMContentLoaded', () => {
     // we want to test if keys pressed work
     // then assign direction of navigation to keys
     function isKeyPressed(yes){
-        cubes[position].classList.remove('snake');
+        cubes[lastIndex].classList.remove('snake');
 
         if(yes.keyCode === 38){
             // alert('up');
